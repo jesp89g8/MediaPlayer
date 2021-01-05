e1b7a9b82c448a5919b433ca73b7ace19900ccd1
jesp89g8 <jesp89g8@easv365.dk>
2021-01-05 20:20:53 +0100

    added handle methods in Controller and getSongs in Main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,48 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main extends Application {
+    final String GUI = "MediaPlayer.fxml";
+    final String TITLE = "MediaPlayer";
+    final String LISTVIEWSONG_FXID = "#listviewSong";
+    final int RES_WIDTH = 1200;
+    final int RES_HEIGHT = 800;
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource(GUI));
+        primaryStage.setTitle(TITLE);
+        primaryStage.setScene(new Scene(root, RES_WIDTH, RES_HEIGHT));
         primaryStage.show();
+
+        getSongs(primaryStage);
     }
 
+    private void getSongs(Stage primaryStage) {
+        ListView<String> listviewSong = (ListView<String>) primaryStage.getScene().lookup(LISTVIEWSONG_FXID);
+        ArrayList<String> song = new ArrayList<>();
+
+        /*
+        get songs from db,
+        add them to song ArrayList,
+        insert them into listviewSong
+         */
+
+    }
 
     public static void main(String[] args) {
         launch(args);
