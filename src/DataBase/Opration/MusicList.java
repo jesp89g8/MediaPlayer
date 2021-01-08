8dd7bbd2273d8a94632e4750606c39f51de172e5
Evens <feix0033@easv365.dk>
2021-01-08 12:53:11 +0100

    create some class without contants 07-01-2021 by Fei

---
+++ src/DataBase/Opration/MusicList.java
@@ -0,0 +1,18 @@
+package DataBase.Opration;
+
+/**
+ * @ author Fei Gu
+ * @ create 2021-01-08-12.31
+ * @ grade CS20_EASV_SØNDERBORG
+ * @ Description
+ * @ Version
+ */
+public class MusicList {
+    private int musicID;
+    private String musicName;
+    private String Artist;
+    private String Path;
+
+
+
+}
---
+++ src/DataBase/Opration/SongList.java
@@ -0,0 +1,11 @@
+package DataBase.Opration;
+
+/**
+ * @ author Fei Gu
+ * @ create 2021-01-08-12.32
+ * @ grade CS20_EASV_SØNDERBORG
+ * @ Description
+ * @ Version
+ */
+public class SongList {
+}
