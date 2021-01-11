9a6105379e453af659a0e8517b320307bd9054c8
Evens <feix0033@easv365.dk>
2021-01-11 13:27:23 +0100

    test

--- src/DataBase/Opration/PlayList.java
+++ src/DataBase/Opration/PlayList.java
@@ -1,5 +1,7 @@
 package DataBase.Opration;
 
+import DataBase.SQL;
+
 /**
  * @ author Fei Gu
  * @ create 2021-01-08-12.32
@@ -12,6 +14,8 @@ public class PlayList {
     private String playListName;
 
     public int getPlayListID() {
+
+        Integer.parseInt(playListTest);
         return playListID;
     }
 
@@ -24,6 +28,8 @@ public class PlayList {
     }
 
     public void setPlayListName(String playListName) {
+
+        String playlistTest =  SQL.selectSQL("select fldPlaylistID from table_Playlist where fldPlaylistName = %s" + playListName).get(0);
         this.playListName = playListName;
     }
 }
